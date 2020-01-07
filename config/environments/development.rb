f9fd4bde6b15e174167fc57d3c6159a7fd432b59
sb8001at <arayatakao@gmail.com>
2020-01-07 01:09:02 +0000

    Construct Users database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "3edfe2cd38fb4cb2b3d7adbaad2ab534.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
